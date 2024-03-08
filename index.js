25411ce925aaa8d39f8915490a883bb3b2678a7c
Chryssa Karatza <chryssakaratza@gmail.com>
2024-03-08 15:27:40 +0000

    change the ip return

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', function (req, res) {
 
 // your first API endpoint...
 app.get('/api/whoami', function (req, res) {
-    let serverIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    let serverIp = req.socket.remoteAddress;
     let serverLanguage = req.headers['accept-language'];
     let serverSoftware = req.headers['user-agent'];
     
